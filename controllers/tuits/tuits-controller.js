6cc71810331c02a799c14128c7a0c4387c52203d
zhugelijuan123 <nancyrain2015@163.com>
2023-03-25 23:06:41 -0700

    node server code

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -8,7 +8,9 @@ const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
+    newTuit.dislikes = 0;
     newTuit.liked = false;
+    newTuit.disliked = false;
     newTuit.image = "https://www.reuters.com/resizer/S2QZTkoqSfT_OagpUN2LL5FX5j8=/960x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/TXDGVXPBEVMY5PA2RHRF2EA25Y.jpg";
     tuits.push(newTuit);
     res.json(newTuit);
