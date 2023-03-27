993e5d10e61b5c0895f6bf64d67279aee71757c3
zhugelijuan123 <nancyrain2015@163.com>
2023-03-26 21:55:23 -0700

    fix explore page-add default info at newtuit

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -9,7 +9,7 @@ export default [
 {
  "_id": "234", "topic": "Traffic", "username": "The Boring Company",
  "handle": "@boringcompany", "time": "2h",
-  "image": "https://www.reuters.com/resizer/S2QZTkoqSfT_OagpUN2LL5FX5j8=/960x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/TXDGVXPBEVMY5PA2RHRF2EA25Y.jpg",
+  "image": "../images/head1.jpeg",
  "title": "The Boring Company fixing traffic",
  "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
  "liked": true, "likes": 2345, "replies": 321, "retuits": 432,"dislikes":1233,"disliked":false
